54ae5da58ee433daeb5ae320312c753aef02cb4d
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2024-08-18 22:46:22 -0400

    Change how SD model is downloaded by adding a bash script which corrects the issue on MacOS/Linux

---
+++ resources/scripts/download_sd_model.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"$1" -c "import torch; from diffusers import AutoPipelineForInpainting; model = AutoPipelineForInpainting.from_pretrained('runwayml/stable-diffusion-inpainting', torch_dtype=torch.float32); model.save_pretrained('$2')"
